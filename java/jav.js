233ea1e48b8a1db1c17c80aa964c4258256b25dc
Basil <rizeigat@gmail.com>
2024-12-13 14:13:55 +0200

    first commit

---
+++ java/jav.js
@@ -0,0 +1,7 @@
+/*================================ Typing animation ========================================*/
+var typed = new Typed(".typing",{
+	strings:["Web Developer","Programmer","Systems Analyst","Database design"],
+	typeSpeed:100,
+	BackSpeed:60,
+	loop:true
+})
